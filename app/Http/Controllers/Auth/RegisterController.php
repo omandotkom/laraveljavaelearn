260a5e865392888628bb538fe70d1e07e02863ce
Khalid Abdurrahman <omandotkom@gmail.com>
2020-09-05 16:59:42 +0700

    fixregister controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,7 +73,7 @@ class RegisterController extends Controller
 
         $user->role = "admin";
         }
-    $user->save();
+         $user->save();
         return $user;
         /*$user = new User();
         $user->name =
