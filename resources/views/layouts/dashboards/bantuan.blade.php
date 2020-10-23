e37e5456aa31f464e1a1c52edb83cf2359a85407
Khalid Abdurrahman <omandotkom@gmail.com>
2020-10-23 14:10:28 +0700

    kerapihan content

--- resources/views/layouts/dashboards/bantuan.blade.php
+++ resources/views/layouts/dashboards/bantuan.blade.php
@@ -2,13 +2,13 @@
     <div class="card">
         <div class="card-body">
             <div class="d-flex">
-                <h4 class="card-title">@if(isset($num)) <b style="font-size:25px;">@php echo $num; @endphp</b> @endif Bantuan</h4>
+                <h4 class="card-title">@if(isset($num)) <b style="font-size:25px;">@php echo $num; @endphp</b> @endif Petunjuk</h4>
             </div>
             <div class="d-flex align-items-center flex-row mt-30">
                 <img src="{{url('dashboardasset/img/help.png')}}" class="rounded img-fluid mx-auto d-block" alt="...">
             </div>
             <div class="d-flex align-items-center flex-row mt-30">
-                <a type="button" role="button" href="{{route('help')}}" class="btn mx-auto btn-outline-info"></i>Bantuan</a>
+                <a type="button" role="button" href="{{route('help')}}" class="btn mx-auto btn-outline-info"></i>Petunjuk</a>
             </div>
         </div>
     </div>
