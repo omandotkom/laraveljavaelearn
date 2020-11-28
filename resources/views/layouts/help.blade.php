3780ad77e23b27f6e60f0fdcc7f2a63e024d794b
unknown <omandotkom@gmail.com>
2020-11-28 22:08:57 +0700

    commit

--- resources/views/layouts/help.blade.php
+++ resources/views/layouts/help.blade.php
@@ -9,11 +9,11 @@
             @elseif(Auth::user()->role == "student")
             <ol>
                 <li>Siswa login terlebih dahulu menggunakan e-mail dan password yang telah terdaftar.</li>
-                <li>Siswa mengakses menu materi untuk melihat dan mengunduh materi.</li>
-                <li>Siswa mengakses menu latihan koding untuk latihan terlebih dahulu sebelum mengisi quiz</li>
-                <li>Siswa mengakses menu soal untuk mengerjakan quiz yang tersedia</li>
-                <li>Siswa mengakses menu evaluasi nilai untuk melihat hasil nilai yang didapat</li>
                 <li>Siswa mengakses menu manajemen kelas untuk melihat tata tertib kelas yang sudah ditentukan oleh Instruktur.</li>
+                <li>Siswa mengakses menu materi untuk melihat dan mengunduh materi.</li>
+                <li>Siswa mengakses menu latihan koding untuk latihan terlebih dahulu sebelum mengisi quiz.</li>
+                <li>Siswa mengakses menu menu soal untuk mengerjakan quiz yang tersedia.</li>
+                <li>Siswa mengakses menu evaluasi nilai untuk melihat hasil nilai yang didapat.</li>
             </ol>
             @endif
         </div>
